8492b226cd57a7d76d7b241a90edef19ca1b1561
dbreon88 <dbreon88@gmail.com>
2023-01-24 16:01:12 -0800

    caching to s3, keep script tags, basic auth, etc. TASKID: prerender

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var prerender = require('./lib');
 
 var server = prerender();
 
+server.use(prerender.basicAuth())
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.browserForceRestart());
 // server.use(prerender.blockResources());
-server.use(prerender.removeScriptTags());
-server.use(prerender.httpHeaders());
+if (process.env.REMOVE_SCRIPT_TAGS) {
+    server.use(prerender.removeScriptTags());
+}
+server.use(prerender.replaceCachedAssetUrls());
+server.use(prerender.httpHeaders()); 
 
 server.start();
